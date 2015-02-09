b11a2856b99f2ec1926515282b0d3439b0ba8e57
Kikojas Deployer <deployer@kikojas.com>
2015-02-10 02:52:58 +0330

    Added cedarmaps MapView attribution

--- CedarMapsTestApp/src/main/java/com/cedarstudios/cedarmaps/sample/Constants.java
+++ CedarMapsTestApp/src/main/java/com/cedarstudios/cedarmaps/sample/Constants.java
@@ -3,6 +3,6 @@ package com.cedarstudios.cedarmaps.sample;
 public class Constants {
 
     public static final String MAPID_CEDARMAPS_STREETS = "cedarmaps.streets";
-    public static final String CLIENT_ID = "user";
-    public static final String CLIENT_SECRET = "pass";
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
 }
